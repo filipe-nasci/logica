c443df1438fd72d16e2755e22069665b0cd72af1
filipe-nasci <filipevaladao19@gmail.com>
2020-06-14 18:54:13 -0300

    atualizando exercicios

---
+++ api3.js
@@ -0,0 +1,8 @@
+const axios = require("axios")
+var rs = require("readline-sync")
+var episodios = rs.question("digite o numero do episodio")
+
+axios.get(`https://www.breakingbadapi.com/api/episodes/${episodios}`)
+.then(function (resposta) {
+    console.log(resposta.data)})
+    
