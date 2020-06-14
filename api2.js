280ee5b074a891222fd5a7c129c1da56ac67bb65
filipe-nasci <filipevaladao19@gmail.com>
2020-06-14 18:25:19 -0300

    atualizando exercicios

---
+++ api2.js
@@ -0,0 +1,5 @@
+//[{"quote":"Seriously? \"Hello Kitty\"?","author":"Jesse Pinkman"}]
+
+const axios = require("axios")
+axios.get("https://breaking-bad-quotes.herokuapp.com/v1/quotes")
+.then(function (resposta){console.log(resposta.data)})
